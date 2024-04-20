a00a09ba3e515ead9df1f43f0b496875a17209ce
ZeroCal Su <3146312184@qq.com>
2024-04-20 17:48:48 +0800

    优化设置逻辑

--- apps/Setting.js
+++ apps/Setting.js
@@ -1,7 +1,5 @@
 import plugin from "../../../lib/plugins/plugin.js"
-import Skland from "../components/Code.js"
 import Config from "../components/Config.js"
-import Log from "../utils/logs.js"
 
 export class Setting extends plugin {
     constructor() {
@@ -11,97 +9,64 @@ export class Setting extends plugin {
             priority: 1009,
             rule: [
                 {
-                    reg: "^#?(skland|(明日)?方舟)(开启|关闭)(自动签到|体力推送)$",
-                    fnc: "userSetting"
+                    reg: "^#?(skland|(明日)?方舟)(开启|关闭)自动签到$",
+                    fnc: "setAutoSign"
+                },
+                {
+                    reg: "^#?(skland|(明日)?方舟)(开启|关闭)理智推送$",
+                    fnc: "setAutoPush"
                 }
             ]
         })
-        this.task = [
-            {
-                name: '[Skland-plugin]自动签到',
-                fnc: async () => this.autoSignIn(),
-                cron: '0 4 * * *',
-                log: true
-            },
-            {
-                name: '[Skland-plugin]体力推送',
-                fnc: () => this.autoPush(),
-                cron: '7 * * * *',
-                log: true
-            }
-        ]
     }
 
-    async userSetting(e) {
-        let config = Config.getConfig()
-        const { user_id: userId, self_id: selfId } = e
-        const [, value, key] = /(开启|关闭)(自动签到|体力推送)$/.exec(e.msg) || []
-        if (!value || !key) return await e.reply('请检查配置指令')
-        let alterFlag = false
-        switch (key) {
-            case '自动签到':
-                const indexSignIn = config["skland_auto_signin_list"].indexOf(`${selfId}:${userId}`)
-                if (value === '开启' && indexSignIn === -1) {
-                    config.push(`${selfId}:${userId}`)
-                    alterFlag = true
-                } else if (value === '关闭' && indexSignIn !== -1) {
-                    config.splice(`${selfId}:${userId}`, 1)
-                    alterFlag = true
-                }
-                break
-            case '体力推送':
-                const indexPush = config['skland_push_list'].indexOf(userId)
-                if (value === '开启' && indexPush === -1) {
-                    config.push(userId)
-                    alterFlag = true
-                } else if (value === '关闭' && indexPush !== -1) {
-                    config.splice(userId, 1)
-                    alterFlag = true
-                }
-                break
-            default:
+    async setAutoSign(e) {
+        const accountList = JSON.parse(await redis.get(`Yunzai:skland:${e.user_id}`)) || await Config.getUserConfig(e.user_id);
+        if (!accountList.length) return e.reply("你还没有绑定任何账号呢，请先绑定账号");
+
+        const config = await Config.getConfig();
+        const key = `${e.self_id}:${e.group_id}:${e.user_id}`;
+        const index = config["skland_auto_signin_list"].indexOf(key);
+
+        if (e.msg.includes('开启')) {
+            if (index === -1) {
+                config["skland_auto_signin_list"].push(key);
+                Config.setConfig(config);
+                return e.reply("已开启自动签到");
+            }
+            return e.reply("你已经开启了自动签到");
         }
-        try {
-            if (alterFlag === true) {
-                Config.setConfig(config)
-            }
-            e.reply(`用户:${userId} 已${value}${key}`)
-            Log.m(`[skland-plugin]更新配置`, userId, key, value)
-        } catch (err) {
-            e.reply("配置文件修改失败，请查看控制台")
-            Log.e(err)
+
+        if (index !== -1) {
+            config["skland_auto_signin_list"].splice(index, 1);
+            Config.setConfig(config);
+            return e.reply("已关闭自动签到");
         }
-        return true
+        return e.reply("你已经关闭了自动签到");
     }
 
-    async autoPush() {
-        const { skland_push_list: pushList } = Config.getConfig()
-        await Promise.allSettled(pushList.map(async user => {
+    async setAutoPush(e) {
+        const accountList = JSON.parse(await redis.get(`Yunzai:skland:${e.user_id}`)) || await Config.getUserConfig(e.user_id);
+        if (!accountList.length) return e.reply("你还没有绑定任何账号呢，请先绑定账号");
 
-        }))
-    }
+        const config = await Config.getConfig();
+        const key = `${e.self_id}:${e.group_id}:${e.user_id}`;
+        const index = config["skland_auto_signin_list"].indexOf(key);
 
-    async autoSignIn() {
-        const { skland_auto_signin_list: autoSignInList } = Config.getConfig()
-        let successNumberList = await Promise.allSettled(autoSignInList.map(async user => {
-            const accountList = JSON.parse(await redis.get(`Yunzai:skland:${user}`)) || await Config.getUserConfig(user)
-            if (accountList.length === 0) {
-                return null
+        if (e.msg.includes('开启')) {
+            if (index === -1) {
+                config["skland_auto_push_list"].push(key);
+                Config.setConfig(config);
+                return e.reply("已开启理智推送");
             }
-            const sklandInstance = new Skland()
-            let successNumber = 0
-            for (let item of accountList) {
-                let uidList = item.uid;
-                let token = item.token
-                for (let uid of uidList) {
-                    await sklandInstance.runSignIn(uid, token).then(res => {
-                        if (res.status) {
-                            successNumber++
-                        }
-                    })
-                }
-            }
-            Log.m('今日完成自动签到数量: ' + successNumberList.filter(number => number).reduce((total, num) => total + num, 0))
-        }))
+            return e.reply("你已经开启了理智推送");
+        }
+
+        if (index !== -1) {
+            config["skland_auto_push_list"].splice(index, 1);
+            Config.setConfig(config);
+            return e.reply("已关闭理智推送");
+        }
+        return e.reply("你已经关闭了理智推送");
     }
 }
